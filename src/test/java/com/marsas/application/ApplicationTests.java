382d940a69ee9fc79ba2e8a4ce426b97a245ae04
Andrii Prybyla <apryby@softserveinc.com>
2023-09-01 13:33:33 +0300

    testing first controller

--- src/test/java/com/marsas/application/ApplicationTests.java
+++ src/test/java/com/marsas/application/ApplicationTests.java
@@ -1,7 +1,12 @@
 package com.marsas.application;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
+    @Test
+    public void applicationStarts() {
+        Application.main(new String[] {});
+    }
 }
